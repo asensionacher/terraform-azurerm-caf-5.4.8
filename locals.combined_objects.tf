278ddaf95319c096c0625790094e94cbd64e717c
Sergi Asensio <sergi.asensio@bravent.net>
2021-12-15 15:01:26 +0100

    ootro más

--- locals.combined_objects.tf
+++ locals.combined_objects.tf
@@ -36,7 +36,7 @@ locals {
   combined_objects_dedicated_host_groups                          = merge(tomap({ (local.client_config.landingzone_key) = module.dedicated_host_groups }), try(var.remote_objects.dedicated_host_groups, {}))
   combined_objects_dedicated_hosts                                = merge(tomap({ (local.client_config.landingzone_key) = module.dedicated_hosts }), try(var.remote_objects.dedicated_hosts, {}))
   combined_objects_diagnostic_storage_accounts                    = merge(tomap({ (local.client_config.landingzone_key) = module.diagnostic_storage_accounts }), try(var.remote_objects.diagnostic_storage_accounts, {}))
-  combined_objects_function_apps_access_policy                    = merge(tomap({ (local.client_config.landingzone_key) = module.function_apps_access_policy  }), try(var.remote_objects.function_apps_access_policy , {}))
+  combined_objects_function_apps                    = merge(tomap({ (local.client_config.landingzone_key) = module.function_apps  }), try(var.remote_objects.function_apps , {}))
   combined_objects_disk_encryption_sets                           = merge(tomap({ (local.client_config.landingzone_key) = module.disk_encryption_sets }), try(var.remote_objects.disk_encryption_sets, {}))
   combined_objects_dns_zones                                      = merge(tomap({ (local.client_config.landingzone_key) = module.dns_zones }), try(var.remote_objects.dns_zones, {}))
   combined_objects_event_hub_namespaces                           = merge(tomap({ (local.client_config.landingzone_key) = module.event_hub_namespaces }), try(var.remote_objects.event_hub_namespaces, {}))
